dfe717c2dde4e49fd2ccdad2e9ad55c320e0c7a9
Atakan Göçer <atakangocer@hotmail.com>
2021-04-19 18:17:28 +0300

    users information added

--- CORE/Utilities/Security/JWT/AccessToken.cs
+++ CORE/Utilities/Security/JWT/AccessToken.cs
@@ -8,5 +8,6 @@ namespace CORE.Utilities.Security.JWT
     {
         public string Token { get; set; }
         public DateTime Expiration { get; set; }
+        public int UserId { get; set; }
     }
 }
--- CORE/Utilities/Security/JWT/JWTHelper.cs
+++ CORE/Utilities/Security/JWT/JWTHelper.cs
@@ -35,6 +35,7 @@ namespace CORE.Utilities.Security.JWT
             {
                 Token = token,
                 Expiration = _accessTokenExpiration,
+                UserId=user.Id
             };
 
         }
